33c3ad3ed1c37331a0c0d33a9a598fea409c861b
connsteele <35544211+connsteele@users.noreply.github.com>
2025-08-01 21:58:17 -0700

    Blog posting - Fetch data from local jsons and display them directly onto the BlogWall component
    Posts will be move into card components that are styled nicely, for now I just need to test things out

--- src/components/BlogWall.jsx
+++ src/components/BlogWall.jsx
@@ -1,15 +1,41 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import style from "../styles/BlogWall.module.css"
 
-const data = {};
 
 const BlogWall = () => {
-    const [posts, setPosts] = useState(data);
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try {
+                const response = await fetch("/posts/connor-review-1.json");
+                const data = await response.json();
+                console.log(data);
+                setPosts([...posts, data]);
+                
+            } catch (e) {
+                console.error(e);
+            }
+        };
+
+        getPosts();
+    }, []);
 
     return(
         <div className={style["blog-wall"]}>
             <ul>
-                {}
+                {posts.map((post) => (
+                    <li>
+                        <h1>{post.title}</h1>
+                        {post.coverImg ? <img src={post.coverImg} alt={post.book + " book cover"} /> : null}
+                        <p>Book: {post.book}</p> 
+                        { post.series ? <p>Series: {post.series}</p> : null}
+                        <p>Author: {post.book}</p>
+                        { post.rating ? <p>Rating: {post.rating} / 5.00</p> : null}
+                        <p>Reviewer: {post.poster}</p>
+                        <p>{post.content}</p>
+                    </li>
+                ))}
             </ul>
         </div>
     );
