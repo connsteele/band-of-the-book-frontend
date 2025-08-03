b791ceb57e74905ad8f79c4f298879b8bba82e48
connsteele <35544211+connsteele@users.noreply.github.com>
2025-08-02 19:01:35 -0700

    Good post layout using grid, but cannot animate
    Saving before if I decide to change posts back to flex where they can animate max height

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,27 +1,32 @@
+import { use, useState } from "react";
 import style from "../styles/Post.module.css"
 
-const MAX_WORDCOUNT = 250;
+const charLimit = 285;
 
 // For blog wall
 const Post = ({post}) => {
+    const [expand, setExpand] = useState(false);
+    const [styleNames, setStyleNames] = useState(`${style.post}`);
     const postTitle = `${post.type[0].toLocaleUpperCase()}${post.type.slice(1)}: ${post.book}`
 
     return (
-        <li className={style.post}>
+        <li className={styleNames}>
             <h1 className={style["post-title"]}>{postTitle}</h1>
             <div className={style["book-info"]}>
+                <p>by {post.author}</p>
                 {post.series ? <p><em>{post.series}</em></p> : null}
-                <p>By: {post.author}</p>
             </div>
             {post.coverImg ? <img src={post.coverImg} alt={post.book + " book cover"} /> : null}
             <div className={style["post-info"]}>
                 <p><b>Blogger:</b> {post.blogger}</p>
                 {post.rating ? <p>Rating: {post.rating} / 5.00</p> : null}
             </div>
-            {post.content.length > MAX_WORDCOUNT ?
+            {(post.content.length > charLimit && !expand) ?
                 <span className={style["post-content"]}>
-                    <p >{post.content.slice(0, MAX_WORDCOUNT) + "..."}</p>
-                </span> :
+                    <p >{post.content.slice(0, charLimit) + "..."}</p>
+                    <p className={style["read-more"]} onClick={() => {setExpand(true); setStyleNames(`${style.post} ${style.expand}`);}}>Read More</p>
+                </span> : (post.content.length > charLimit && expand) ?
+                <span className={style["post-content"]}><p>{post.content}</p></span> :
                  <p className={style["post-content"]}>{post.content}</p>
             }
         </li>
